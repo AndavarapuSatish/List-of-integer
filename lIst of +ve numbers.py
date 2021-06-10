64bc49737c1e75f4793728668ead120b5ca394a0
AndavarapuSatish <85400168+AndavarapuSatish@users.noreply.github.com>
2021-06-10 21:05:58 +0530

    Add files via upload

---
+++ lIst of +ve numbers.py
@@ -0,0 +1,9 @@
+list1 = [12,-14,34,-56,64]
+list2 = [12,141,-50,32,-78]
+for num in list1:
+  if num >=0:
+    print(num , end = " ")
+print("\n")
+for num1 in list2:
+  if num1 >=0:
+    print(num1 , end = " ")
